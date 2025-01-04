d99f22c8191fb3cdfa1e8601d62d344bf98f6dfb
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-01-04 18:02:13 +0530

    Completed Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,25 @@
+
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+import Header from "@/components/Header";
+import PageTransitions from "@/components/PageTransitions";
+import Stairs from "@/components/ui/Stairs";
+import StairTransition from "@/components/ui/StairTransition";
+
+const JetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  variable: "--font-jetbrainsMono",
+
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +34,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${JetBrainsMono.variable}  antialiased`}
       >
-        {children}
+        <Header/>
+        <StairTransition/>
+        <PageTransitions>
+          {children}
+
+        </PageTransitions>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,59 @@
 import type { Config } from "tailwindcss";
+import { DEFAULT_CIPHERS } from "tls";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: '15px'
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '960px',
+			xl: '1200px'
+		},
+		fontFamily: {
+			primary: "var(--font-jetbrainsMono)"
+		},
+		extend: {
+			colors: {
+				primary:'#1c1c22',
+				accent:{
+					DEFAULT:'#00ff99',
+					hover:'#00e187'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			animation: {
+				meteor: 'meteor 5s linear infinite'
+			},
+			keyframes: {
+				meteor: {
+					'0%': {
+						transform: 'rotate(215deg) translateX(0)',
+						opacity: '1'
+					},
+					'70%': {
+						opacity: '1'
+					},
+					'100%': {
+						transform: 'rotate(215deg) translateX(-500px)',
+						opacity: '0'
+					}
+				}
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
