e725fc0dd69b449953682eab968c56dbce943d8d
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-01-05 16:10:31 +0530

    Completed Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Button } from '@/components/ui/button'
 import { FiDownload } from 'react-icons/fi'
 import Socials from '@/components/Socials'
+import Photo from '@/components/Photo'
+import Stats from '@/components/Stats'
 const Home = () => {
   return (
-    <section className='h-full'>
+    <section className='min-h-screen'>
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className='text-xl
             '>Software Developer</span>
             <h1 className='h1'>
@@ -17,6 +19,7 @@ const Home = () => {
 
             <div className='flex flex-col xl:flex-row items-center gap-8'>
               <Button variant='outline' size='lg' className='uppercase flex items-center gap-2'>
+                
                 <span>Download CV</span>
                 <FiDownload className='text-xl'/>
               </Button>
@@ -25,8 +28,13 @@ const Home = () => {
               </div>
             </div>
           </div>
+
+          <div className='order-1 xl:order-none mb-8 xl:mb-0'>
+              <Photo/>
+          </div>
         </div>
       </div>
+      <Stats/>
     </section>
   )
 }
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,12 @@
-import React from 'react'
+"use client"
 
+import {FaHtml5,FaCss3,FaJs,FaReact,FaNodeJs} from "react-icons/fa"
+import { SiTailwindcss,SiNextdotjs} from "react-icons/si"
+
+const about = {
+  title:"About me"
+  description
+}
 const page = () => {
   return (
     <div>
