dbf7933fb531a63e56d01a60692542cb68b86e8a
Himanshu-Khairnar <himanshuk1205@gmail.com>
2025-01-06 17:02:49 +0530

    Completed Resume

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -40,7 +40,9 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full
+    bg-accent dark:bg-slate-800 bg-border" />
+
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
